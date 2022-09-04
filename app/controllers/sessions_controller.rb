e41079bcb30861853ae9d95b4e7e777250f7d697
Paul Tobin <tobster111@gmail.com>
2022-09-04 09:55:10 +0100

    ternery to check the state of the checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       reset_session
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
