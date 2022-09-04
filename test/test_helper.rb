a70ea793566414b27b3ad84e03598b1464084aec
Paul Tobin <tobster111@gmail.com>
2022-09-04 10:04:41 +0100

    test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,17 +8,27 @@ Minitest::Reporters.use!
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-    include ApplicationHelper
 
-    # Add more helper methods to be used by all tests here...
     # Returns true if a test user is logged in.
     def is_logged_in?
       !session[:user_id].nil?
+    end
+
+    # Log in as a particular user.
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
+  end
+end
+
+module ActionDispatch
+  class IntegrationTest
+    # Log in as a particular user.
+    def log_in_as(user, password: 'password', remember_me: '1')
+      post login_path, params: { session: { email: user.email,
+                                            password: password,
+                                            remember_me: remember_me } }
     end
   end
 end
